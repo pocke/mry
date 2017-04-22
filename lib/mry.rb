2a6954521eb4a28b2244f3baa1a1007fd91d8157
Masataka Kuwabara <kuwabara@pocke.me>
2017-04-22 19:32:13 +0900

    Add a rewriter for RuboCop 0.48.0

--- lib/mry.rb
+++ lib/mry.rb
@@ -3,5 +3,4 @@ require 'yaml_rewriter'
 require "mry/version"
 
 module Mry
-  # Your code goes here...
 end
---
+++ lib/mry/rewriter_0_47_0.rb
@@ -0,0 +1,8 @@
+class Rewriter_0_47_0 < YAMLRewriter::Rewriter
+  define_rule ['Lint/Eval' => 'Security/Eval']
+  define_rule ['Style/CaseIndentation', 'IndentWhenRelativeTo' => 'EnforcedStyle']
+  define_rule ['Lint/BlockAlignment', 'AlignWith' => 'EnforcedStyleAlignWith']
+  define_rule ['Lint/EndAlignment', 'AlignWith' => 'EnforcedStyleAlignWith']
+  define_rule ['Lint/DefEndAlignment', 'AlignWith' => 'EnforcedStyleAlignWith']
+  define_rule ['Rails/UniqBeforePluck', 'EnforcedMode' => 'EnforcedStyle']
+end
